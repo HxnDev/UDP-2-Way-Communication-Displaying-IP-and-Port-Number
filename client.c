c0b0c83423de3e0a1ac00a28967044a08e4938ee
Hassan Shahzad <83294673+HxnDev@users.noreply.github.com>
2021-07-10 11:17:33 +0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,52 @@
+// udp client driver program 
+#include <stdio.h> 
+#include <strings.h> 
+#include <sys/types.h> 
+#include <arpa/inet.h> 
+#include <sys/socket.h> 
+#include<netinet/in.h> 
+#include<unistd.h> 
+#include<stdlib.h> 
+
+#define PORT 5000 
+#define MAXLINE 1000 
+
+// Driver code 
+int main() 
+{ 
+	char buffer[100]; 
+	char *message = "Hello Server"; 
+	int sockfd, n; 
+	struct sockaddr_in servaddr; 
+	
+	// clear servaddr 
+	bzero(&servaddr, sizeof(servaddr)); 
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_port = htons(PORT); 
+	servaddr.sin_family = AF_INET; 
+	
+	// create datagram socket 
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0); 
+	
+	// connect to server 
+	if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) 
+	{ 
+		printf("\n Error : Connect Failed \n"); 
+		exit(0); 
+	} 
+
+	// request to send datagram 
+	// no need to specify server address in sendto 
+	// connect stores the peers IP and port 
+	sendto(sockfd, message, MAXLINE, 0, (struct sockaddr*)&servaddr, sizeof(servaddr)); 
+	
+	// waiting for response 
+	recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&servaddr, NULL); 
+	puts(buffer); 
+
+	printf("Server's IP Number on Client's Side = %s", inet_ntoa(servaddr.sin_addr));
+	printf("Server's Port Number on Client's Side = %d", (int)ntohs(servaddr.sin_port));
+	
+	// close the descriptor 
+	close(sockfd); 
+} 
